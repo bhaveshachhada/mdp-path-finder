45eba9b9d7e38f6b9f3fefd16ee09ed115efdee7
bhavesh <bhaveshachhada.ab@gmail.com>
2022-10-09 20:38:13 +0530

    :memo: added render method to environment

--- environment.py
+++ environment.py
@@ -1,5 +1,8 @@
 from typing import Dict, List, Tuple
 
+import cv2
+import numpy as np
+
 import constants
 from cell import Cell
 
@@ -29,10 +32,15 @@ class Environment:
         self._read_environment_config()
         self._generate_grid()
 
+        self.agent_position = self.start_position
+
+        self._image = None
+        # self.render()
+
     def _read_environment_config(self):
         f = open(self._filename, 'r')
 
-        count = 1
+        count = 0
 
         for line in f:
 
@@ -100,6 +108,50 @@ class Environment:
         i, j = self.start_position
         return self._grid[i][j]
 
+    def render(self):
+        cell_size = (100, 100)
+        border_width = 2
+        self._image = np.zeros((*cell_size, 3), dtype=np.uint8)
+        for i in range(self.n_rows):
+            row_image = None
+            for j in range(self.n_cols):
+                if (i, j) == self.start_position:
+                    R, G, B = 200, 20, 20
+                elif (i, j) == self.goal_position:
+                    R, G, B = 20, 200, 20
+                elif (i, j) in self.obstacle_positions:
+                    R, G, B = 20, 20, 20
+                else:
+                    R, G, B = 128, 128, 128
+                b = np.full(cell_size, B)
+                g = np.full(cell_size, G)
+                r = np.full(cell_size, R)
+                cell_image = cv2.merge((b, g, r))
+                cell_image[:, :border_width, :] = (255, 255, 255)
+                cell_image[:border_width, :, :] = (255, 255, 255)
+                cell_image[-border_width:, :, :] = (255, 255, 255)
+                cell_image[:, -border_width:, :] = (255, 255, 255)
+
+                if (i, j) == self.agent_position:
+                    cv2.circle(cell_image,
+                               (cell_size[0] // 2, cell_size[1] // 2),
+                               min(cell_size) // 4,
+                               (255, 20, 20),
+                               -1)
+
+                if j == 0:
+                    row_image = cell_image
+                else:
+                    row_image = np.hstack((row_image, cell_image))
+            if i == 0:
+                self._image = row_image
+            else:
+                self._image = np.vstack((self._image, row_image))
+        self._image = self._image.astype(np.uint8)
+        print(f'{self._image.dtype}, {self._image.shape}')
+        cv2.imshow('output', self._image)
+        cv2.waitKey(0)
+
 
 def main():
     filename = 'testcases/ex1.txt'
@@ -107,6 +159,7 @@ def main():
     assert env.start_position is not None
     assert env.drift_ccw_probabilities is not None
     assert env.drift_cw_probabilities is not None
+    env.render()
 
 
 if __name__ == '__main__':
