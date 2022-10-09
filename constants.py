48b850639085b0c0cd4f3486c1960e48306d25d6
bhavesh <bhaveshachhada.ab@gmail.com>
2022-10-09 15:55:48 +0530

    :memo: minor changes

--- constants.py
+++ constants.py
@@ -3,15 +3,15 @@ from enum import Enum, unique
 
 @unique
 class Move(Enum):
-    UP = 1
-    DOWN = 2
+    FORWARD = 1
+    BACKWARD = 2
     SPIN_RIGHT = 3
     SPIN_LEFT = 4
 
 
 ACTION_BASE_COST = {
-    Move.UP: 1.0,
-    Move.DOWN: 1.0,
+    Move.FORWARD: 1.0,
+    Move.BACKWARD: 1.0,
     Move.SPIN_LEFT: 1.0,
     Move.SPIN_RIGHT: 1.0
 }
